bb9193820150b4a2e311bc1e84713d193397f8fd
ShaikhSohrab <shaikhsohrab96@gmail.com>
2024-09-24 14:11:39 +0400

    removed skip

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -1,6 +1,6 @@
 import {test} from "@playwright/test";
 
-test.skip("Basic Navigation", async ({page}) => {
+test("Basic Navigation", async ({page}) => {
     await page.goto("https://gitlab.com/");
     await page.click("a.slp-btn.be-nav__nav-button.slp-btn-primary");
     // await page.locator('[data-testid="new-user-first-name-field"]').fill('John1');
@@ -11,6 +11,6 @@ test.skip("Basic Navigation", async ({page}) => {
     
 })
 
-test.skip("Using Various Locator Methods", async({page})=>{
+// test.skip("Using Various Locator Methods", async({page})=>{
 
-})
+// })
